c4e63388f867bf0a02d4a2c412cd5a5ca415068a
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-30 18:33:21 +0100

    `black .`

--- homework_10/settings.py
+++ homework_10/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,8 +55,7 @@ ROOT_URLCONF = "homework_10.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,3 +124,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_BROKER_URL = "amqp://guest@localhost//"
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
--- sms/urls.py
+++ sms/urls.py
@@ -3,6 +3,4 @@ from django.urls import path
 from sms import views
 
 
-urlpatterns = [
-    path("sms/", views.sms)
-]
+urlpatterns = [path("sms/", views.sms)]
